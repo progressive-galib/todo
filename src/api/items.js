0ecb586733524aa1fd4cd0b41ac21f8586fae687
progressive-galib <125106402+progressive-galib@users.noreply.github.com>
2023-11-01 01:19:54 +0600

    get method added and tested

--- src/api/items.js
+++ src/api/items.js
@@ -1,4 +1,16 @@
 const express = require("express")
+const cassandra = require('cassandra-driver'); require('dotenv').config();
+
+const { NODE1, NODE2, NODE3, LOCAL_DATA_CENTER, USERNAME, PASSWORD } =
+    process.env;
+
+const cluster = new cassandra.Client({
+    contactPoints: [NODE1, NODE2, NODE3],
+    localDataCenter: LOCAL_DATA_CENTER,
+    credentials: { username: USERNAME, password: PASSWORD },
+    keyspace: 'avengers',
+});
+
 
 const items = [
     {id:1, name:"fetch schooby snacks", completed:"false"},
@@ -9,7 +21,9 @@ const items = [
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.json({items})
+    const result = cluster.execute('SELECT * FROM todo_items');
+    res.json(result.rows);
+
 })
 
 module.exports = router;
